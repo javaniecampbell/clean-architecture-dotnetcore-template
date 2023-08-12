33bbbfcfbeae88562ad7976df97ed3beac5f7d7e
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2023-08-11 20:07:51 -0500

    feat: registered dependencies for infrastructure and application layers

--- CleanArchitectureTemplate.Api/Program.cs
+++ CleanArchitectureTemplate.Api/Program.cs
@@ -1,7 +1,13 @@
+using CleanArchitectureTemplate.Infrastructure;
+using CleanArchitectureTemplate.Application;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services
+    .AddApplication()
+    .AddInfrastructure();
+    
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
