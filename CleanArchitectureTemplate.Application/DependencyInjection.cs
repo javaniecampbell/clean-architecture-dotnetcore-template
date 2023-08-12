52b284bfd5922a1278bcbdbae9921c31f1ff5d25
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2023-08-11 20:05:34 -0500

    refactor: renamed namespace for application layer

--- CleanArchitectureTemplate.Application/DependencyInjection.cs
+++ CleanArchitectureTemplate.Application/DependencyInjection.cs
@@ -1,6 +1,6 @@
 using Microsoft.Extensions.DependencyInjection;
 
-namespace CleanArchitectureTemplate.Infrastructure;
+namespace CleanArchitectureTemplate.Application;
 
 
 public static class DependencyInjection
