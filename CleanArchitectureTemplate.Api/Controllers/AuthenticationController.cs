b8a63e2495aac000a8a8c6871b6f01828b0af9a5
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2023-08-14 23:59:38 -0500

    feat: injected authentication service into presentation layer from application layer

--- CleanArchitectureTemplate.Api/Controllers/AuthenticationController.cs
+++ CleanArchitectureTemplate.Api/Controllers/AuthenticationController.cs
@@ -1,3 +1,4 @@
+using CleanArchitectureTemplate.Application.Services.Authentication;
 using CleanArchitectureTemplate.Contracts.Authentication;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,23 +10,54 @@ public class AuthenticationController : ControllerBase
 {
     
     private readonly ILogger<AuthenticationController> _logger;
+    private readonly IAuthenticationService _authenticationService;
 
-    public AuthenticationController(ILogger<AuthenticationController> logger)
+    public AuthenticationController(ILogger<AuthenticationController> logger, IAuthenticationService authenticationService)
     {
         _logger = logger;
+        _authenticationService = authenticationService;
     }
+    
 
     [HttpPost("register")]
     public async Task<IActionResult> Register(RegisterRequest request)
     {
         await Task.Delay(1000);
-        return Ok(request);
+        var result = _authenticationService.Register(
+            request.FirstName,
+            request.LastName,
+            request.Email,
+            request.Password
+        );
+
+        var response = new AuthenticationResponse(
+            result.Id,
+            result.FirstName,
+            result.LastName,
+            result.Email,
+            result.Token
+        );
+
+        return Ok(response);
     }
 
     [HttpPost("login")]
     public async Task<IActionResult> Login(LoginRequest request)
     {
         await Task.Delay(1000);
-        return Ok(request);
+        var result = _authenticationService.Login(
+            request.Email,
+            request.Password
+        );
+
+        var response = new AuthenticationResponse(
+            result.Id,
+            result.FirstName,
+            result.LastName,
+            result.Email,
+            result.Token
+        );
+
+        return Ok(response);
     }
 }
